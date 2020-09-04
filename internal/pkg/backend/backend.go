ff38dd05122e48a64c99d2b793a11740b4cbd4e6
Daniil Rutskiy <dstdfx@gmail.com>
2020-09-04 16:17:33 +0300

    Add backend implementation

---
+++ internal/pkg/backend/backend.go
@@ -0,0 +1,33 @@
+package backend
+
+import (
+	"time"
+
+	"github.com/dstdfx/bookish-spork/internal/pkg/config"
+	"github.com/dstdfx/bookish-spork/internal/pkg/qqcache"
+	"go.uber.org/zap"
+)
+
+// Backend contains common application dependencies.
+type Backend struct {
+	Log   *zap.Logger
+	Cache *qqcache.Cache
+}
+
+// New init new Backend instance.
+func New(log *zap.Logger) (*Backend, error) {
+	opts := qqcache.Opts{
+		EvictionInterval: time.Duration(config.Config.Cache.EvictionInterval) * time.Second,
+	}
+
+	return &Backend{
+		Log:   log,
+		Cache: qqcache.New(opts),
+	}, nil
+}
+
+// Shutdown method closes all backend connections.
+func (b *Backend) Shutdown() {
+	b.Log.Debug("backend shutdown")
+	b.Cache.Shutdown()
+}
---
+++ internal/pkg/backend/backend_test.go
@@ -0,0 +1,33 @@
+package backend
+
+import (
+	"testing"
+
+	"github.com/dstdfx/bookish-spork/internal/pkg/config"
+	"github.com/dstdfx/bookish-spork/internal/pkg/log"
+	"github.com/dstdfx/bookish-spork/internal/pkg/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBackend(t *testing.T) {
+	// Check acceptance test flag
+	if !testutils.IsAccTestEnabled(t) {
+		return
+	}
+
+	// Init global app configuration
+	testutils.InitTestConfig()
+
+	// Initialize logger
+	logger, err := log.InitLogger(log.InitLoggerOpts{
+		Debug:     config.Config.Log.Debug,
+		UseStdout: config.Config.Log.UseStdout,
+		File:      config.Config.Log.File,
+	})
+	assert.NoError(t, err)
+
+	b, err := New(logger)
+	defer b.Shutdown()
+	assert.NoError(t, err)
+	assert.NotNil(t, b)
+}
